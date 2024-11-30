c03f047f884f5914a9c8a316ca73a24fefeaa230
Maciej Buszko <m@maciej.me>
2024-11-29 17:36:12 -0800

    use cli parameters

--- src/bp/two_level_adaptive.cc
+++ src/bp/two_level_adaptive.cc
@@ -9,6 +9,7 @@
 extern "C" {
 #include "bp/bp.param.h"
 #include "core.param.h"
+#include "bp.param.h"
 #include "globals/assert.h"
 #include "statistics.h"
 }
@@ -29,7 +30,7 @@ enum Automata_state {S0 = 0, S1, S2, S3};   // Enumeration of automata states
 
 // ___Adjustable parameters___ (for experimentation)
 // Size of each entry in history register table
-#define HRT_entry_size 12
+#define HRT_entry_size TLA_HRT_ENTRY_SIZE
 
 // The maximum number of entries in the history register table
 // TODO: This does nothing. We will need to write some way for the entries over
@@ -39,7 +40,7 @@ enum Automata_state {S0 = 0, S1, S2, S3};   // Enumeration of automata states
 // history register table), which is essentially an LRU cache. 
 // The HHRT is essentially a direct mapped cache (possibly without address 
 // checking), so this size can be used during its initialization.
-#define HRT_size 512
+#define HRT_size TLA_HRT_ENTRIES
 
 // The number of buckets in the ideal history register table. This is not to be
 // confused with the number of entries in the pattern table.
@@ -47,7 +48,7 @@ enum Automata_state {S0 = 0, S1, S2, S3};   // Enumeration of automata states
 #define IHRT_buckets 4096
 
 // The automata to use for the pattern table
-#define PT_automata LAST_TIME
+#define PT_automata TLA_AUTOMATA
 
 // Derived parameters
 // A bit mask for n bits should be 2^n - 1
@@ -59,109 +60,151 @@ enum Automata_state {S0 = 0, S1, S2, S3};   // Enumeration of automata states
 namespace {
   struct TLA_State {
     Hash_Table ihr_table;    // ihr_table = IHRT (Ideal History Register Table)
-    uns64 hash_hr_table[];  
-    // This is misleading, it's more like a direct mapped cache, but the paper 
+
+    // This is misleading, it's more like a direct mapped cache, but the paper
     // uses that name, so we'll also use it (to reduce confusion)
     // This may be replaced by the AHRT (Address History Register Table) when
     // that is implemented
+    std::vector<uns64> hash_hr_table;
+
     std::vector<uns8> pattern_table;
   };
 
   std::vector<TLA_State> tla_state_all_cores;
 
-  // ---------- History Register Table ----------
+  // ---------- Hash History Register Table ----------
   // Note: The current implementation is based on a hash table. We can probably
   // do a similar thing to the automata and add a wrapper to allow for a cache
-  // implementation. -> In reality, the hash table implementation didn't account 
+  // implementation. -> In reality, the hash table implementation didn't account
   // for limited HRT size and resembled the IHRT (ideal history register table)
   // more. The functionality will commented out (but kept for the IHRT).
 
-  // ihrt_init: Initialize the underlying hash table for the history register table.
-  // We can setting all entries to 0 at the time of the first update. Before that, 
-  // the hash table accesses return NULL if the entry is not found, which the 
-  // wrapper function returns as 0.
-  //void ihrt_init(TLA_State* tla_state) {
-  //  init_hash_table(&tla_state->hr_table,
-  //    "history_register_table", HRT_buckets, sizeof(uns64));
-  //}
-
   // ihrt_init: Initialize the HHRT with its proper size and all contents to 0.
   void hhrt_init(TLA_State* tla_state) {
-    &tla_state->hash_hr_table = uns64[HRT_size]; // TODO: Check if this is right
+    tla_state->hash_hr_table.resize(HRT_size, 0);
+  }
 
-    for (int i = 0; i < HRT_size; i++) {
-      &tla_state->hash_hr_table[i] = 0;
-    }
-  }                                                
 
-  // ihrt_get: Get the ideal history register content for the given address
-  // Don't need to handle collisions (since the IHRT is used to simulate the 
-  // theoretical scenario of every branch being able to have its own HR)
-  // uns64 ihrt_get(const TLA_State* tla_state, const Addr addr) {
-  //  void* data = hash_table_access(&tla_state->ihr_table, addr);
+  // Collisions: The paper mentions "interference" in the execution history
+  // when using the HHRT. It may be possible that this is referring to the
+  // fact that the HRRT have no address checking, so if 2 branch addresses
+  // share the same hash, they may end up using the same HR entry. I find this
+  // somewhat unlikely, but I'll assume that for now. (The AHRT has address
+  // checking, so I would expect if they wanted to simulate a direct-mapped
+  // HRT with address checking, they would have at least 1 setup with a 1-way
+  // set associative AHRT.)
 
-  //  if (!data) {
-  //    return 0;  // Return 0 if the entry is not found
-  //  }
-
-  //  const uns64* history = static_cast<uns64*>(data);
+  // hhrt_get: Get the ideal history register content for the given address
+  uns64 hhrt_get(const TLA_State* tla_state, const Addr addr) {
+    // TODO: Make sure this is right!!
+    int HHRT_index = static_cast<int>(addr) % HRT_size;
+    // void* data = &tla_state->hash_hr_table[HHRT_index];
+    // no collision checking for now (so expect performance to be... bad)
+    const uns64 history = tla_state->hash_hr_table.at(HHRT_index);
 
     // Return the last HRT_entry_size bits of the history register content
     // This removes any history older than HRT_entry_size, effectively working
     // as though the underlying data structure has HRT_entry_size bits.
-  //  return *history & HRT_entry_mask;
-  //} 
-  
-  // hhrt_get: Get the ideal history register content for the given address
+    return history & HRT_entry_mask;
+  }
 
-  // Collisions: The paper mentions "interference" in the execution history
-  // when using the HHRT. It may be possible that this is referring to the 
-  // fact that the HRRT have no address checking, so if 2 branch addresses 
-  // share the same hash, they may end up using the same HR entry. I find this
-  // somewhat unlikely, but I'll assume that for now. (The AHRT has address
-  // checking, so I would expect if they wanted to simulate a direct-mapped 
-  // HRT with address checking, they would have at least 1 setup with a 1-way
-  // set associative AHRT.) 
-  uns64 hhrt_get(const TLA_State* tla_state, const Addr addr) {
+  // ihrt_update: Update the hash history register content for the given address
+  // with  the branch outcome.
+  void hhrt_update(TLA_State* tla_state, const Addr addr, const uns8 outcome) {
     // TODO: Make sure this is right!!
-    int HHRT_index = static_cast<int>(addr) % HRT_size;     
-    void* data = &tla_state->hash_hr_table[HHRT_index];
+    int   HHRT_index = static_cast<int>(addr) % HRT_size;
+    auto* history    = &tla_state->hash_hr_table[HHRT_index];
     // no collision checking for now (so expect performance to be... bad)
+
+    // Left shift and insert the latest outcome bit
+    *history = (*history << 1) | (outcome & 0x1);
+  }
+
+  // ---------- Ideal History Register Table ----------
+
+  // ihrt_init: Initialize the underlying hash table for the history register
+  // table. We can setting all entries to 0 at the time of the first update.
+  // Before that, the hash table accesses return NULL if the entry is not found,
+  // which the wrapper function returns as 0.
+  void ihrt_init(TLA_State* tla_state) {
+    init_hash_table(&tla_state->ihr_table, "history_register_table", IHRT_buckets,
+                    sizeof(uns64));
+  }
+
+  // ihrt_get: Get the ideal history register content for the given address
+  // Don't need to handle collisions (since the IHRT is used to simulate the
+  // theoretical scenario of every branch being able to have its own HR)
+  uns64 ihrt_get(const TLA_State* tla_state, const Addr addr) {
+    void* data = hash_table_access(&tla_state->ihr_table, addr);
+
+    if(!data) {
+      return 0;  // Return 0 if the entry is not found
+    }
+
     const uns64* history = static_cast<uns64*>(data);
 
     // Return the last HRT_entry_size bits of the history register content
     // This removes any history older than HRT_entry_size, effectively working
     // as though the underlying data structure has HRT_entry_size bits.
     return *history & HRT_entry_mask;
-  } 
+  }
 
-  // ihrt_update: Update the history register content for the given address with 
-  // the branch outcome. The other implementation used the hash_table_access_replace 
-  // function which might be a better choice here as well, though I think that this
-  // also works.
-//  void ihrt_update(TLA_State* tla_state, const Addr addr, const uns8 outcome) {
-    //Flag found;
-    //auto* history = static_cast<uns64*>(
-    //  hash_table_access_create(&tla_state->ihr_table, addr, &found));
+  // ihrt_update: Update the history register content for the given address with
+  // the branch outcome. The other implementation used the
+  // hash_table_access_replace function which might be a better choice here as
+  // well, though I think that this also works.
+  void ihrt_update(TLA_State* tla_state, const Addr addr, const uns8 outcome) {
+    Flag  found;
+    auto* history = static_cast<uns64*>(
+      hash_table_access_create(&tla_state->ihr_table, addr, &found));
 
-    //if (!found) {
-    //  *history = 0;  // Initialize new entries to 0
-    //}
+    if(!found) {
+      *history = 0;  // Initialize new entries to 0
+    }
 
     // Left shift and insert the outcome bit
-//    *history = (*history << 1) | (outcome & 0x1);
-//  }
-
-  // ihrt_update: Update the hash history register content for the given address 
-  // with  the branch outcome.  
-  void hhrt_update(TLA_State* tla_state, const Addr addr, const uns8 outcome) {
-    // TODO: Make sure this is right!!
-    int HHRT_index = static_cast<int>(addr) % HRT_size;     
-    auto* history = &tla_state->hash_hr_table[HHRT_index];
-    // no collision checking for now (so expect performance to be... bad)
-
-    // Left shift and insert the latest outcome bit
     *history = (*history << 1) | (outcome & 0x1);
+  }
+
+  // ---------- History Register Selection Mechanism ----------
+
+  // Select the appropriate function to call depending on which underlying
+  // mechanism is being used for the history register table.
+  void hrt_init(TLA_State* tla_state) {
+    if(TLA_HRT_MECHANISM == 0) {
+      return ihrt_init(tla_state);
+    } else if (TLA_HRT_MECHANISM == 1) {
+      return hhrt_init(tla_state);
+    } else if (TLA_HRT_MECHANISM == 2) {
+      return ihrt_init(tla_state);
+    } else {
+      ASSERT(0, "Invalid HRT mechanism");
+    }
+  }
+
+  uns64 hrt_get(const TLA_State* tla_state, const Addr addr) {
+    if(TLA_HRT_MECHANISM == 0) {
+      return ihrt_get(tla_state, addr);
+    } else if (TLA_HRT_MECHANISM == 1) {
+      return hhrt_get(tla_state, addr);
+    } else if (TLA_HRT_MECHANISM == 2) {
+      return ihrt_get(tla_state, addr);
+    } else {
+      ASSERT(0, "Invalid HRT mechanism");
+      return 0;
+    }
+  }
+
+  void hrt_update(TLA_State* tla_state, const Addr addr, const uns8 outcome) {
+    if(TLA_HRT_MECHANISM == 0) {
+      return ihrt_update(tla_state, addr, outcome);
+    } else if (TLA_HRT_MECHANISM == 1) {
+      return hhrt_update(tla_state, addr, outcome);
+    } else if (TLA_HRT_MECHANISM == 2) {
+      return ihrt_update(tla_state, addr, outcome);
+    } else {
+      ASSERT(0, "Invalid HRT mechanism");
+    }
   }
 
   // ---------- Pattern Table ----------
@@ -213,26 +256,22 @@ namespace {
   // Automata A1: Records results of last 2 times that history pattern appeared
   // A1 Update function: Record new history pattern as leftmost bit
   Automata_state automata_A1_update(Automata_state state, const uns8 latest_result) {
-
     switch(state) {
       case S0: if (latest_result == 0) {
                 return S0;
               } else {
                 return S1;
               }
-
       case S1: if (latest_result == 0) {
                 return S2;
               } else {
                 return S3;
               }
-
       case S2: if (latest_result == 0) {
                 return S0;
               } else {
                 return S1;
               }
-
       case S3: if (latest_result == 0) {
                 return S2;
               } else {
@@ -262,30 +301,22 @@ namespace {
               } else {
                 return S1;
               }
-              //break;
-
       case S1: if (latest_result == 0) {
                 return S0;
               } else {
                 return S2;
               }
-              //break;
-
       case S2: if (latest_result == 0) {
                 return S1;
               } else {
                 return S3;
               }
-              //break;
-
       case S3: if (latest_result == 0) {
                 return S2;
               } else {
                 return S3;
               }
-              //break;
       default: return S0;
-              //break;
     }
   }
 
@@ -293,15 +324,10 @@ namespace {
   uns8 automata_A2_get_pred(Automata_state state) {
     switch (state) {
       case S0: return 0;
-              //break;
       case S1: return 0;
-              //break;
       case S2: return 1;
-              //break;
       case S3: return 1;
-              //break;
       default: return 0;
-              //break;
     }
   }
 
@@ -314,30 +340,22 @@ namespace {
               } else {
                 return S1;
               }
-              //break;
-
       case S1: if (latest_result == 0) {
                 return S0;
               } else {
                 return S3;
               }
-              //break;
-
       case S2: if (latest_result == 0) {
                 return S3;
               } else {
                 return S0;
               }
-              //break;
-
       case S3: if (latest_result == 0) {
                 return S2;
               } else {
                 return S3;
               }
-              //break;
       default: return S0;
-              //break;
     }
   }
 
@@ -345,15 +363,10 @@ namespace {
   uns8 automata_A3_get_pred(Automata_state state) {
     switch (state) {
       case S0: return 0;
-              //break;
       case S1: return 0;
-              //break;
       case S2: return 1;
-              //break;
       case S3: return 1;
-              //break;
       default: return 0;
-              //break;
     }
   }
 
@@ -366,30 +379,22 @@ namespace {
               } else {
                 return S1;
               }
-              //break;
-
       case S1: if (latest_result == 0) {
                 return S0;
               } else {
                 return S3;
               }
-              //break;
-
       case S2: if (latest_result == 0) {
                 return S1;
               } else {
                 return S3;
               }
-              //break;
-
       case S3: if (latest_result == 0) {
                 return S2;
               } else {
                 return S3;
               }
-              //break;
       default: return S0;
-              //break;
     }
   }
 
@@ -397,15 +402,10 @@ namespace {
   uns8 automata_A4_get_pred(Automata_state state) {
     switch (state) {
       case S0: return 0;
-              //break;
       case S1: return 0;
-              //break;
       case S2: return 1;
-              //break;
       case S3: return 1;
-              //break;
       default: return 0;
-              //break;
     }
   }
 
@@ -417,22 +417,15 @@ namespace {
   // Note: This replaced the change_SC function from the earlier versions.
 
   uns8 automata_update(const uns8 state, const uns8 latest_result) {
-    Automata_state AT_state = static_cast<int>(state);  // uns8 -> int   Cast into int        
-    Automata_state updated_state;    // for automata state enum (for update function)
+    Automata_state AT_state = static_cast<Automata_state>(state);
     switch (PT_automata) {
-      case LAST_TIME: automata_last_time_update(AT_state, latest_result);
-      case A1: updated_state = automata_A1_update(AT_state, latest_result);
-              break;
-      case A2: updated_state = automata_A2_update(AT_state, latest_result);
-              break;
-      case A3: updated_state = automata_A3_update(AT_state, latest_result);
-              break;
-      case A4: updated_state = automata_A4_update(AT_state, latest_result);
-              break;
-      default: updated_state = 0;
-              break;
+      case LAST_TIME: return automata_last_time_update(AT_state, latest_result);
+      case A1: return automata_A1_update(AT_state, latest_result);
+      case A2: return automata_A2_update(AT_state, latest_result);
+      case A3: return automata_A3_update(AT_state, latest_result);
+      case A4: return automata_A4_update(AT_state, latest_result);
+      default: return 0;
     }
-    return static_cast<uns8>(updated_state);   // int -> uns8
   }
 
   // Get the prediction for the branch by calling the currently selected automata 
@@ -440,24 +433,16 @@ namespace {
   // changing the state).
 
   uns8 automata_get_pred(const uns8 state) {
-    Automata_state AT_state = static_cast<int>(state);   // uns8-> int  
-    uns8 current_state;               
+    Automata_state AT_state = static_cast<Automata_state>(state);
 
     switch (PT_automata) {     // get_pred automatically returns uns8
-      case LAST_TIME: current_state = automata_last_time_get_pred(AT_state);
-              break;
-      case A1: current_state = automata_A1_get_pred(AT_state);
-              break;
-      case A2: current_state = automata_A2_get_pred(AT_state);
-              break;
-      case A3: current_state = automata_A3_get_pred(AT_state);
-              break;
-      case A4: current_state = automata_A4_get_pred(AT_state);
-              break;
+      case LAST_TIME: return automata_last_time_get_pred(AT_state);
+      case A1: return automata_A1_get_pred(AT_state);
+      case A2: return automata_A2_get_pred(AT_state);
+      case A3: return automata_A3_get_pred(AT_state);
+      case A4: return automata_A4_get_pred(AT_state);
       default: return 0;
-              break;
     }
-    return current_state;
   }
 
 } //  end of namespace
@@ -478,7 +463,7 @@ uns8 bp_two_level_adaptive_pred(Op* op) {
   const uns   proc_id       = op->proc_id;  // process ID
   const auto& tla_state     = tla_state_all_cores.at(proc_id);
   const Addr  addr          = op->oracle_info.pred_addr;  // instruction address
-  
+
   // Looking here, we have what appears to be the prediction history for the
   // branch. We actually might not even need to have a history register table,
   // since we could just index into the pattern table with the history (well,
@@ -489,7 +474,7 @@ uns8 bp_two_level_adaptive_pred(Op* op) {
 
   const uns64 history = hrt_get(&tla_state, addr);
   const uns8 automata_state = pt_get(&tla_state, history);
-  const uns8 prediction_bit = automata_get(automata_state);
+  const uns8 prediction_bit = automata_get_pred(automata_state);
 
   return prediction_bit;
 
@@ -540,9 +525,4 @@ void bp_two_level_adaptive_update(Op* op) {
   const uns8 new_automata_state = automata_update(automata_state, outcome);
   pt_update(&tla_state, history, new_automata_state);
   hrt_update(&tla_state, addr, outcome);
-
-  // HRT.update_HR_content(addr, hist);  // FIGURE OUT HOW TO GET LATEST PREDICTION
-  // const uns HR_bits = HRT.get_HR_content(addr);
-  // BHPT.change_SC(history_register_table.get_HR_content(addr),
-  //                get_pattern_history_bits(HR_bits), 0);
 }
